af8cf04512e69f639a6345ad6b18188aa674bde7
Romeo Fermano Jr. <junfermano@gmail.com>
2026-02-23 15:43:33 +0800

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.javafx.univesitymanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.javafx.univesitymanagementsystem to javafx.fxml;
+    exports com.javafx.univesitymanagementsystem;
+}
